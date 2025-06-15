390a708c1ff0de3ff3d1121f0dbddcb70c540474
Victor Chen <victorc@squareup.com>
2025-06-15 10:59:32 -0400

    Update snippet refresh

--- refresh_snippet.py
+++ refresh_snippet.py
@@ -1,76 +1,91 @@
 """
-This script iterates all folders under current path, and for each ".ly" file, remove its associated PDF and MIDI files,
-and execute LilyPond command to re-generate them.
+1. Reads snippet config from snippet.json
+2. For each snippet, generate the ./snippet/name.ly file based on template_snippet.ly
+3. Generate the PNG and MP3 files via LilyPond and FluidSynth commands
+4. Clean up intermediate files
 """
+
 import os
-import subprocess
 import json
+import subprocess
 
 from pydub import AudioSegment
+
+SNIPPET_CONFIG = 'snippet.json'
+SNIPPET_TEMPLATE = 'template_snippet.ly'
+SNIPPET_DIR = 'snippet'
 
 LILYPOND_PATH = 'D:/Programs/lilypond-2.24.3/bin/lilypond.exe'
 FLUIDSYNTH_PATH = 'D:/Programs/fluidsynth-2.4.6/bin/fluidsynth.exe'
 SOUNDFONT_PATH = 'D:/Piano/Steinway_Model_D274.sf2'
 
 def refresh():
-    root_dir = os.path.join(os.getcwd(), 'snippet')
-    processed_files = []
+  for snippet in read_snippets():
+    refresh_snippet(snippet)
 
-    for _, _, files in os.walk(root_dir):
-        for file in files:
-            if file.endswith('.ly'):
-                refresh_file(root_dir, file)
+def read_snippets():
+  with open(SNIPPET_CONFIG, 'r') as f:
+    snippets = json.load(f)
+  return snippets
 
-                # Extract filename without extension
-                name = os.path.splitext(file)[0]
-                processed_files.append({
-                    'name': name,
-                    'png': f'snippet/{name}.png',
-                    'mp3': f'snippet/{name}.mp3'
-                })
+def refresh_snippet(snippet):
+  generate_ly(snippet)
+  generate_files(snippet)
 
-    # Generate snippet.json
-    json_path = os.path.join(os.getcwd(), 'snippet.json')
-    with open(json_path, 'w', encoding='utf-8') as f:
-        json.dump(processed_files, f, indent=2, ensure_ascii=False)
+def generate_ly(snippet):
+  with open(SNIPPET_TEMPLATE, 'r') as f:
+    template = f.read()
 
-def refresh_file(dir, file):
-    name = os.path.splitext(file)[0]
+  template = template.replace('{name}', snippet['name'])
+  template = template.replace('{tempo}', snippet['tempo'])
+  template = template.replace('{key}', snippet['key'])
 
-    pdf = os.path.join(dir, f'{name}.pdf')
-    mid = os.path.join(dir, f'{name}.mid')
-    wav = os.path.join(dir, f'{name}.wav')
-    mp3 = os.path.join(dir, f'{name}.mp3')
-    png = os.path.join(dir, f'{name}.png')
-    cropped_png = os.path.join(dir, f'{name}.cropped.png')
+  for i, snippet_part in enumerate(snippet['snippets']):
+    # Make a copy of the template
+    template_copy = template
+    template_copy = template_copy.replace('{snippet}', snippet_part)
 
-    # Remove all existing files before generating new ones
-    os.path.exists(pdf) and os.remove(pdf)
-    os.path.exists(mid) and os.remove(mid)
-    os.path.exists(wav) and os.remove(wav)
-    os.path.exists(mp3) and os.remove(mp3)
-    os.path.exists(png) and os.remove(png)
-    os.path.exists(cropped_png) and os.remove(cropped_png)
+    file_path = os.path.join(SNIPPET_DIR, f'{snippet["name"]} {i+1}.ly')
+    with open(file_path, 'w') as f:
+      f.write(template_copy)
 
-    # Generate MIDI, PNG and cropped PNG
-    command = f'{LILYPOND_PATH} -fpng -dresolution=500 -dcrop -s -o "{dir}" "{os.path.join(dir, file)}"'
-    subprocess.run(command, shell=True)
+def generate_files(snippet):
+  name = snippet['name']
 
-    # Generate WAV from MIDI
-    command = f'{FLUIDSYNTH_PATH} -q -n -i -r 44100 -F "{wav}" "{SOUNDFONT_PATH}" "{mid}"'
-    subprocess.run(command, shell=True)
+  pdf = os.path.join(SNIPPET_DIR, f'{name}.pdf')
+  mid = os.path.join(SNIPPET_DIR, f'{name}.mid')
+  wav = os.path.join(SNIPPET_DIR, f'{name}.wav')
+  mp3 = os.path.join(SNIPPET_DIR, f'{name}.mp3')
+  png = os.path.join(SNIPPET_DIR, f'{name}.png')
+  cropped_png = os.path.join(SNIPPET_DIR, f'{name}.cropped.png')
 
-    # Generate MP3 from WAV
-    audio = AudioSegment.from_wav(wav)
-    audio.export(mp3, format='mp3')
-    os.remove(wav)
+  # Remove all existing files before generating new ones
+  os.path.exists(pdf) and os.remove(pdf)
+  os.path.exists(mid) and os.remove(mid)
+  os.path.exists(wav) and os.remove(wav)
+  os.path.exists(mp3) and os.remove(mp3)
+  os.path.exists(png) and os.remove(png)
+  os.path.exists(cropped_png) and os.remove(cropped_png)
 
-    # Overwrite the full PNG with the cropped PNG
-    if os.path.exists(png) and os.path.exists(cropped_png):
-        os.remove(png)
-        os.rename(cropped_png, png)
+  # Generate MIDI, PNG and cropped PNG
+  command = f'{LILYPOND_PATH} -fpng -dresolution=500 -dcrop -s -o "{SNIPPET_DIR}" "{os.path.join(SNIPPET_DIR, name)}"'
+  subprocess.run(command, shell=True)
 
-    print(f'Refreshed {name}')
+  # Generate WAV from MIDI
+  command = f'{FLUIDSYNTH_PATH} -q -n -i -r 44100 -F "{wav}" "{SOUNDFONT_PATH}" "{mid}"'
+  subprocess.run(command, shell=True)
+
+  # Generate MP3 from WAV
+  audio = AudioSegment.from_wav(wav)
+  audio.export(mp3, format='mp3' bitrate="128k")
+  os.remove(wav)
+
+  # Overwrite the full PNG with the cropped PNG
+  if os.path.exists(png) and os.path.exists(cropped_png):
+      os.remove(png)
+      os.rename(cropped_png, png)
+
+  print(f'Refreshed {name}')
 
 if __name__ == '__main__':
-    refresh()
+  refresh()
